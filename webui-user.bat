6455d53e1442f2ea4cf967b0dcca01481203c1fd
Yu Zhenchuan <yuadsl3010@gmail.com>
2023-05-21 19:56:52 +0800

    add

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
-
+set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--no-half --api --theme dark
 
 call webui.bat
